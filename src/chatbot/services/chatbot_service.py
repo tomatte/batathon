13beb39b8980a5842761fbe969b576d4eac95da2
Daniel <daniel.reboucas@loft.com.br>
2025-06-14 14:16:23 -0300

    fix: return str as answer

--- src/chatbot/services/chatbot_service.py
+++ src/chatbot/services/chatbot_service.py
@@ -24,4 +24,4 @@ class ChatbotService:
         answer = await self.fast_agent[agent_name].generate(conversation)
         last_text = answer.last_text()
         self.conversation_service.add_message(message.phone, last_text, AuthorEnum.ASSISTANT)
-        return answer.last_text
+        return last_text
